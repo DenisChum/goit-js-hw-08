04ef5c4f549db8d190937300df3d80e9d49914b6
DenisChum <105285389+DenisChum@users.noreply.github.com>
2022-08-07 19:11:34 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(galleryItems);
+
+const galleryCollection = document.querySelector(".gallery");
+const galleryMarkup = galleryItems.map((item) => `<li class="gallerry__list"><a class="gallery__item" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`).join("");
+
+galleryCollection.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom'})
+
