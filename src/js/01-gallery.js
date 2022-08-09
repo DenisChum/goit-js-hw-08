c6b823c5f3ca723c2b13ba8158c2415493454285
DenisChum <105285389+DenisChum@users.noreply.github.com>
2022-08-09 11:51:51 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,13 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 
 console.log(galleryItems);
 
-import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const galleryCollection = document.querySelector(".gallery");
 const galleryMarkup = galleryItems.map((item) => `<li class="gallerry__list"><a class="gallery__item" href="${item.original}">
